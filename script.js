5748505f509800b002706ee11c831832002bee70
Aldis <123507042+AldisBask@users.noreply.github.com>
2024-01-06 16:43:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateLength() {
+  const centimetriInput = document.getElementById('centimetriInput').value;
+  const centimetri = parseFloat(centimetriInput);
+
+  if (!isNaN(centimetri)) {
+    const olekts = centimetri / 53.75;
+    const collas = centimetri / 2.54;
+    const peds = centimetri / 30.48;
+
+    const resultMessage = `Rezultāti:<br>${centimetri} centimetri ir ${olekts.toFixed(2)} olektis, ${collas.toFixed(2)} collas, vai ${peds.toFixed(2)} pēdas.`;
+
+    document.getElementById('result').innerHTML = resultMessage;
+  } else {
+    document.getElementById('result').innerHTML = 'Lūdzu, ievadiet derīgu skaitli.';
+  }
+}
